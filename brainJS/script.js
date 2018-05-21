7b7ef53f41c7a034fdbd9ce53fa1d8f248d752b8
Siuling Ding <sd@bsdacademy.com>
2018-05-21 09:48:34 +0800

    ...

--- brainJS/script.js
+++ brainJS/script.js
@@ -1,9 +1,9 @@
-let net, r, g, b, bgColour, txtColour, strongCounter = 0, totalMatch = 0, trained = false, trainData = [];
+let net, bgColour, txtColour, strongCombo = 0, totalCombo = 0, minTraining = 30, trained = false, inDemo = false, explicitTraining = false, trainingData = [];
 
 function randomColour() {
-    r = Math.random();
-    g = Math.random();
-    b = Math.random();
+    const r = Math.random();
+    const g = Math.random();
+    const b = Math.random();
 
     return { r, g, b };
 }
@@ -16,59 +16,71 @@ function randomCombo() {
     text.style.color = `rgb(${txtColour.r * 100}%, ${txtColour.g * 100}%, ${txtColour.b * 100}%)`;
 }
 
-function updateText(n) {
-    strongCounter += n;
-    totalMatch++;
-    console.log(strongCounter / totalMatch);
-    info.textContent = `${Math.round((strongCounter / totalMatch) * 1000) / 10}% random chance of getting desirable match`;
+function updateRatio() {
+    inDemo = false;
+    totalCombo++;
+    info.textContent = `${Math.round((strongCombo / totalCombo) * 1000) / 10}% random chance of desirable colour combination`;
 }
 
 window.onload = () => {
+    c3.textContent = minTraining;
+
     net = new brain.NeuralNetwork();
 
-    // fetch JSON from Firebase
-    // net.fromJSON();
-    
+    // const cloudJSON = fetch JSON from Firebase
+    // trainingData = JSON_to_array(cloudJSON)
+    // net.fromJSON(cloudJSON)
+
     randomCombo();
 
     strong.onclick = () => {
         trained = false;
-        trainData.push({input: bgColour, output: txtColour});
-        
+        trainingData.push({ input: bgColour, output: txtColour });
+        demo.disabled = trainingData.length < minTraining;
         randomCombo();
-        demo.disabled = false;
-
-        updateText(1);
+        strongCombo++;
+        updateRatio();
+        c1.textContent = strongCombo;
     };
 
     weak.onclick = () => {
         randomCombo();
-        updateText(0);
+        updateRatio();
+        c2.textContent = totalCombo - strongCombo;
     };
 
     demo.onclick = () => {
+        inDemo = true;
         bgColour = randomColour();
         container.style.backgroundColor = `rgb(${bgColour.r * 100}%, ${bgColour.g * 100}%, ${bgColour.b * 100}%)`;
 
         if (!trained) {
-            net.train(trainData);
+            text.textContent = 'Training network...';
+            if (explicitTraining) trainingData.push({ input: bgColour, output: txtColour });
+            // Firebase.transaction(net.toJSON());
+            net.train(trainingData);
             trained = true;
+            explicitTraining = false;
+            text.textContent = 'Readable Text';
         }
 
         txtColour = net.run(bgColour);
-        console.log(txtColour);
         text.style.color = `rgb(${txtColour.r * 100}%, ${txtColour.g * 100}%, ${txtColour.b * 100}%)`;
+    };
+
+    container.onclick = () => {
+        if (inDemo) {
+            txtColour = randomColour();
+            text.style.color = `rgb(${txtColour.r * 100}%, ${txtColour.g * 100}%, ${txtColour.b * 100}%)`;
+
+            trained = false;
+            explicitTraining = true;
+        }
     };
 };
 
 window.onkeydown = () => {
-    if (event.code == "ArrowLeft") {
-        strong.click();
-    }
-    else if (event.code == "ArrowRight") {
-        weak.click();
-    }
-    else if (event.code == "ArrowUp") {
-        demo.click();
-    }
+    if (event.code == 'ArrowLeft') strong.click();
+    else if (event.code == 'ArrowRight') weak.click();
+    else if (event.code == 'ArrowUp') demo.click();
 };
