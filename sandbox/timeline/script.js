fab3e30f63317b1b9976002c506f3ccc282cd7f8
imding <minddevoursall@gmail.com>
2018-05-30 02:34:52 +0800

    ...

--- sandbox/timeline/script.js
+++ sandbox/timeline/script.js
@@ -1,7 +1,7 @@
 let
   togglePadding = 3, toggleDuration = 120,
   landscape, toggleTransit,
-  sections = [];
+  sections = [], icons = [], nodes = [], nodeTitles = [], nodeDetails = [];
 
 function initStyle() {
   style([toggle], {
@@ -33,11 +33,6 @@ function initStyle() {
     font_weight: 'bold',
     text_align: 'center',
   });
-
-  style(sections, {
-    padding: '50px',
-    text_align: 'center',
-  });
 }
 
 function updateToggle() {
@@ -59,22 +54,69 @@ function updateToggle() {
 }
 
 function updateMode() {
+  style([document.body], {
+    background_size: landscape ? 'auto 100%' : '100%',
+    background_position: landscape ? 'center' : 'initial',
+  });
+
   style([head], {
-    width: landscape ? `${headNode.offsetWidth * 2}px` : '100%',
+    padding: landscape ? '50px' : '80px 25% 50px 25%',
+    width: landscape ? `${headNode.offsetWidth * 3}px` : '100%',
     height: landscape ? `${window.innerHeight - head.offsetTop}px` : 'initial',
   });
 
-  sections.forEach((section, i) => {
-    
-    style([section], {
-      width: section.children
-    });
+  style([headNode], {
+    position: landscape ? 'absolute' : 'initial',
+    top: landscape ? `${((window.innerHeight - banner.offsetHeight) / 2) - (headNode.offsetHeight / 2) + banner.offsetHeight}px` : 'auto',
+    left: landscape ? `${(head.offsetWidth / 2) - (headNode.offsetWidth / 2)}px` : 'auto',
   });
+
+  style([nodeTitles[0]], {
+    position: landscape ? 'absolute' : 'initial',
+    width: landscape ? `${head.offsetWidth - 100}px` : 'auto',
+    bottom: landscape ? `${window.innerHeight - headNode.offsetTop + 10}px` : 'auto',
+  });
+
+  style([nodeDetails[0]], {
+    position: landscape ? 'absolute' : 'initial',
+    width: landscape ? `${nodeTitles[0].offsetWidth}px` : 'auto',
+    top: landscape ? `${headNode.offsetTop + headNode.offsetHeight + 10}px` : 'auto',
+  });
+
+  for (let i = 1; i < sections.length; i++) {
+    const j = i - 1;
+    
+    style([sections[i]], {
+      padding: landscape ? '50px' : '50px 25%',
+      position: landscape ? 'absolute' : 'initial',
+      width: landscape ? `${icons[j].offsetWidth * 2}px` : '100%',
+      height: landscape ? '100%' : 'auto',
+      top: landscape ? '0' : 'auto',
+      left: landscape ? `${sections[j].offsetLeft + sections[j].offsetWidth}px` : 'auto',
+      background_color: i % 2 ? 'slategrey' : 'darkslategrey',
+    });
+    
+    style([nodes[i]], {
+      position: landscape ? 'absolute' : 'initial',
+      top: landscape ? (i > 1 ? `${nodes[j].offsetTop}px` : `${headNode.offsetTop + (headNode.offsetHeight - nodes[i].offsetHeight) / 2}px`) : 'auto',
+      left: landscape ? `${sections[i].offsetWidth / 2 - nodes[i].offsetWidth / 2}px` : 'auto',
+    });
+
+    style([nodeTitles[i]], {
+      position: 'absolute',
+      width: landscape ? `${sections[i].offsetWidth - 100}px` : `${sections[i].offsetWidth * 0.5}px`,
+      bottom: landscape ? `${window.innerHeight - headNode.offsetTop + 10}px` : 'auto',
+    });
+  }
 }
 
 window.onload = function () {
   toggle.onclick = updateToggle;
-  sections = Array.from(document.getElementsByClassName('sections'));
+  sections = document.querySelectorAll('.sections');
+  icons = document.querySelectorAll('.icons');
+  nodes = document.querySelectorAll('.nodes');
+  nodeTitles = document.querySelectorAll('.sections > h2');
+  nodeDetails = document.querySelectorAll('.sections > p');
   initStyle();
   updateMode();
 };
@@ -82,188 +124,7 @@ window.onload = function () {
 function style(elem, declarations) {
   Object.keys(declarations).forEach(d => {
     elem.forEach(e => {
-      if (declarations[d]) e.style[d.replace(/_/g, '-'.toLowerCase())] = declarations[d];
+      if (declarations[d]) e.style[d.replace(/_/g, '-')] = declarations[d];
     });
   });
 }
-
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function createDescription(node) {
-
-  const index = nodes.indexOf(node);
-
-  const textContainer = document.createElement('div');
-
-  textContainer.style.position = 'absolute';
-  textContainer.style.height = '80px';
-  textContainer.style.width = '300px';
-  textContainer.style.textAlign = 'center';
-
-  const nodeMiddle = node.offsetTop + node.offsetHeight / 2;
-  textContainer.style.top = nodeMiddle - 40 + 'px';
-
-  if (index % 2 === 0) { // For even indices
-    textContainer.style.left = node.offsetLeft + (node.offsetWidth / 2) + 80 + 'px';
-  } else { // For odd indices
-    textContainer.style.left = node.offsetLeft + (node.offsetWidth / 2) - 80 - 300 + 'px';
-  }
-
-  const header = document.createElement('h1');
-  header.style.fontFamily = 'arial';
-  header.style.fontSize = '18px';
-  header.textContent = node.children[0].textContent;
-
-  const description = document.createElement('p');
-  description.style.fontFamily = 'arial';
-  description.style.fontSize = '10px';
-  description.textContent = node.children[2].textContent;
-
-  textContainer.appendChild(header);
-  textContainer.appendChild(description);
-
-  document.getElementsByTagName('body')[0].appendChild(textContainer);
-
-
-}
-
-function createIcon(node) {
-
-  const index = nodes.indexOf(node);
-  console.log(index);
-
-  const icon = document.createElement('div');
-
-  icon.style.position = 'absolute';
-  icon.style.height = '80px';
-  icon.style.width = '80px';
-
-  const nodeMiddle = node.offsetTop + node.offsetHeight / 2;
-  icon.style.top = nodeMiddle - 40 + 'px';
-
-  if (index % 2 === 0) {
-    icon.style.left = node.offsetLeft + (node.offsetWidth / 2) - 265 + 'px';
-  } else {
-    icon.style.left = node.offsetLeft + (node.offsetWidth / 2) + 185 + 'px';
-  }
-
-  const iconImage = document.createElement('img');
-
-  iconImage.src = node.children[1].src;
-  iconImage.style.height = '80px';
-  iconImage.style.width = '80px';
-
-  icon.appendChild(iconImage);
-  document.getElementsByTagName('body')[0].appendChild(icon);
-
-
-}
-
-function initLine() {
-  const line = document.getElementById('line');
-
-  line.id = 'line';
-  line.style.position = 'absolute';
-  line.style.width = '10px';
-
-  line.style.left = (window.innerWidth - 10) / 2 + 'px';
-  line.style.zIndex = '-1';
-
-  const lineStart = nodes[0].offsetTop + (nodes[0].offsetHeight / 2);
-  const lastNode = nodes[nodes.length - 1];
-
-  line.style.top = lineStart + 'px';
-  line.style.height = lastNode.offsetTop + (lastNode.offsetHeight / 2) - lineStart + 'px';
-
-
-}
-
-function initBackgroundColors() {
-
-  for (var i = 0; i < nodes.length; i++) {
-
-    const currentNodeCenter = nodes[i].offsetTop + nodes[i].offsetHeight / 2;
-
-    if (i === 0) {
-
-      const nextNodeCenter = nodes[i + 1].offsetTop + nodes[i + 1].offsetHeight / 2;
-      const nextDiff = nextNodeCenter - currentNodeCenter;
-
-      const end = currentNodeCenter + nextDiff / 2;
-
-      const imageBackground = document.createElement('div');
-      imageBackground.style.position = 'absolute';
-      imageBackground.style.width = '100%';
-      imageBackground.style.height = end;
-      imageBackground.style.zIndex = -3;
-
-      const image = document.createElement('img');
-      console.log(nodes[i].children);
-      image.src = nodes[i].children[3].src; // From the kid's input
-      image.style.width = '100%';
-      image.style.height = end;
-
-      imageBackground.appendChild(image);
-
-      document.getElementsByTagName('body')[0].appendChild(imageBackground);
-
-      nodes[i].innerHTML = '';
-      continue;
-    }
-
-
-    const previousNodeCenter = nodes[i - 1].offsetTop + nodes[i - 1].offsetHeight / 2;
-
-    const prevDiff = currentNodeCenter - previousNodeCenter;
-
-    const start = previousNodeCenter + prevDiff / 2;
-
-    // Creating background color sections
-    if (i !== 0 && i !== nodes.length - 1) {
-      const nextNodeCenter = nodes[i + 1].offsetTop + nodes[i + 1].offsetHeight / 2;
-
-      const nextDiff = nextNodeCenter - currentNodeCenter;
-
-      const end = currentNodeCenter + nextDiff / 2;
-
-      const colorBackground = document.createElement('div');
-      colorBackground.style.position = 'absolute';
-      colorBackground.style.width = '100%';
-      colorBackground.style.height = end - start + 'px';
-      colorBackground.style.top = start + 'px';
-      colorBackground.style.backgroundColor = getRandomColor();
-      colorBackground.style.zIndex = -2;
-
-      document.getElementsByTagName('body')[0].appendChild(colorBackground);
-
-    } else if (i === nodes.length - 1) {
-
-      const colorBackground = document.createElement('div');
-      colorBackground.style.position = 'absolute';
-      colorBackground.style.width = '100%';
-      colorBackground.style.height = prevDiff + 'px';
-
-      colorBackground.style.top = start + 'px';
-      colorBackground.style.backgroundColor = getRandomColor();
-      colorBackground.style.zIndex = -2;
-
-      document.getElementsByTagName('body')[0].appendChild(colorBackground);
-
-
-    }
-
-    nodes[i].innerHTML = '';
-
-  }
-
-}
-
-
-
