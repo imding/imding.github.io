ca926c05fb4df0a4677f559a85ef4925d4ab649b
Siuling Ding <sd@bsdacademy.com>
2018-06-27 18:40:45 +0800

    ...

--- sandbox/anim/script.js
+++ sandbox/anim/script.js
@@ -1,17 +1,10 @@
-class Anima {
-    constructor() {
-
-    }
-
-    Add() {
-
-    }
-}
+const uiSections = new Array(6);
+let Explorer, Inspector, Workspace, Preview, Timeline;
 
 function init() {
-
-    
+    uiSection.forEach(section => {
+        section = document.createElement('div');
+    });
 }
-
 
 window.onload = init;
