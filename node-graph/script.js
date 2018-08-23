d77e40d3cf42b5e394a464f0cdc0747b4e4086e6
Siuling Ding <sd@bsdacademy.com>
2018-08-23 20:16:30 +0800

    ...

--- node-graph/script.js
+++ node-graph/script.js
@@ -1,23 +1,248 @@
 loading();
 
+let flo, gCss, gAttr;
+
 function loading() {
     if (document.body) init();
     else window.requestAnimationFrame(loading);
 }
 
 function init() {
+    gCss = new Utility().gCss;
+    gAttr = new Utility().gAttr;
     appContainer.style.visibility = 'visible';
 
-    
+    flo = new FloApp();
+    flo.render(appContainer);
+
+}
+
+class FloApp {
+    constructor() {
+        this.ui = {
+            workspace: [],
+        };
+    }
+
+    render(el) {
+        const
+            checkers = [
+                {
+                    pass: () => arguments.length == 1,
+                    message: 'the render method expects a single argument',
+                },
+                {
+                    pass: () => el && el.tagName == 'DIV',
+                    message: 'the render expects a <div> element',
+                }
+            ];
+
+        checkers.forEach(check => { if (!check.pass()) console.error(check.message); });
+
+        document.newSVG = type => document.createElementNS('http://www.w3.org/2000/svg', type);
+
+        this.el = el;
+
+        const ws = new Workspace(document.createElement('div'));
+
+        this.addWorkspace(ws, { x: 100, y: 200, w: 800, h: 650 });
+    }
+
+    addWorkspace(ws, opts) {
+        if (!(ws instanceof Workspace)) console.error('the addWorkspace method expects an instance of Workspace');
+
+        opts = Object.assign({ x: 0, y: 0, w: 500, h: 350 }, opts);
+
+        const
+            sAttr = new Utility().sAttr,
+            sCss = new Utility().sCss;
+
+        sCss(ws.el, {
+            position: 'absolute',
+            left: `${opts.x}px`,
+            top: `${opts.y}px`,
+            width: `${opts.w}px`,
+            height: `${opts.h}px`,
+        });
+
+        sAttr(ws.svg, {
+            width: opts.w,
+            height: opts.h,
+            viewBox: `0 0 ${opts.w} ${opts.h}`,
+        });
+
+        this.ui.workspace.push(ws);
+        this.el.appendChild(ws.el);
+    }
+}
+
+class Workspace {
+    constructor(el) {
+        const checkers = [
+            {
+                pass: arguments.length < 2,
+                message: 'the Workspace class constructor expects a single argument',
+            },
+            {
+                pass: el.tagName == 'DIV',
+                message: 'the Workspace class constructor expects a <div> element',
+            }
+        ];
+
+        checkers.forEach(check => { if (!check.pass) console.error(check.message); });
+
+        const
+            nzrn = new Utility().nzrn,
+            uid = new Utility().uid,
+            gAttr = new Utility().gAttr,
+            sAttr = new Utility().sAttr,
+            gCss = new Utility().gCss;
+
+        this.el = el;
+        this.el.id = this.el.id || uid('WS');
+        this.el.className = 'AppUI-WS';
+
+        this.svg = document.newSVG('svg');
+        this.svg.id = `${this.el.id}-SVG`;
+
+        this.graph = {
+            nodes: [],
+        };
+
+        this.el.appendChild(this.svg);
+        this.el.oncontextmenu = () => {
+            event.preventDefault();
+            
+            if (event.target != this.svg) return;
+
+            this.addNode({
+                x: event.clientX - gCss(this.el).left,
+                y: event.clientY - gCss(this.el).top,
+            });
+        };
+
+        this.activeNode;
+        this.el.onmousemove = () => {
+            if (!this.activeNode) return;
+            sAttr(this.activeNode.root, {
+                x: event.clientX - this.activeNode.offset.x,
+                y: event.clientY - this.activeNode.offset.y,
+            });
+        };
+    }
+
+    addNode(opts) {
+        const nn = new Node(opts);
+
+        nn.root.onmousedown = () => {
+            if (event.button) return;
+            nn.offset = {
+                x: opts.x - gAttr(nn.root).x,
+                y: 0,
+            };
+            this.activeNode = nn;
+        };
+        nn.root.onmouseup = () => {
+            if (event.button) return;
+            this.activeNode = null;
+        };
+
+        this.svg.appendChild(nn.root);
+        this.graph.nodes.push(nn);
+    }
 }
 
 class Node {
-    constructor(x, y, w, h) {
+    constructor(opts) {
+        opts = Object.assign({
+            x: 0,
+            y: 0,
+            w: 150,
+            h: 100,
+        }, opts || {});
 
+        const sAttr = new Utility().sAttr;
 
-        this.el = document.createElement('g');
-        this.body = document.createElement('rect');
-        
-        this.el.appendChild(this.body);
+        this.root = document.newSVG('g');
+        sAttr(this.root, { cursor: 'pointer' });
+
+        // create and append node body
+        this.body = document.newSVG('rect');
+        sAttr(this.body, {
+            width: opts.w,
+            height: opts.h,
+            x: opts.x - opts.w / 2,
+            y: opts.y - opts.h / 2,
+        });
+        this.root.appendChild(this.body);
+
+        // create and append node input ports
+        this.in = [{
+            svg: document.newSVG('circle'),
+        }];
+        this.in.forEach(port => {
+            sAttr(port.svg, {
+                r: 10,
+                cx: opts.x,
+                cy: opts.y,
+                fill: 'skyblue',
+            });
+            this.root.appendChild(port.svg);
+        });
+
+        // create and append node output ports
+        this.out = [{
+            svg: document.newSVG('circle'),
+        }];
+        this.out.forEach(port => {
+            this.root.appendChild(port.svg);
+        });
+    }
+}
+
+class Utility {
+    constructor() {
+        // non-zero random number
+        this.nzrn = () => Math.random() || this.nzrn();
+
+        // unique id
+        this.uid = prefix => {
+            // random string
+            const rs = `${prefix}-${this.nzrn().toString(36).slice(-3)}`;
+
+            if (Array.from(document.documentElement.getElementsByTagName('*')).some(el => el.id == rs)) return this.uid();
+            return rs;
+        };
+
+        // get item from array
+        this.gifa = (arr, i) => i < 0 ? arr[arr.length + i] : arr[i];
+
+        // set & get attribute
+        this.sAttr = (el, opts) => Object.entries(opts).forEach(kvp => el.setAttribute(kvp[0], kvp[1].toString()));
+        this.gAttr = el => {
+            return new Proxy({
+                get x() { return el.getAttribute('x'); },
+                get y() { return el.getAttribute('y'); },
+            }, {
+                get: (o, attr) => attr in o ? o[attr] : el.getAttribute(attr),
+            });
+        };
+
+        // set & get css style
+        this.sCss = (el, opts) => Object.entries(opts).forEach(kvp => el.style[kvp[0]] = kvp[1]);
+        this.gCss = el => {
+            const
+                cs = window.getComputedStyle(el),
+                val = p => cs.getPropertyValue(p);
+
+            return new Proxy({
+                get width() { return parseFloat(val('width')); },
+                get height() { return parseFloat(val('height')); },
+                get left() { return parseFloat(val('left')); },
+                get top() { return parseFloat(val('top')); },
+            }, {
+                    get: (o, p) => p in o ? o[p] : val(p.replace(/([A-Z])/g, '-$1'.toLowerCase())),
+                });
+        };
     }
 }
