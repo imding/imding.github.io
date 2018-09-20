7a71593442ed4ff0f360c64340c8d7e18fe07bce
Siuling Ding <sd@bsdacademy.com>
2018-09-20 16:21:43 +0800

    ...

--- pipeline/app_dev.js
+++ pipeline/app_dev.js
@@ -13,16 +13,60 @@ function init() {
     sCss(document.body, { visibility: 'visible' });
 
     const nodesData = [
-        'Fundamentals',
+        ['Fundamentals', [
+            'Intro to App Dev',
+            '>> Web Fundamental',
+        ]],
+        ['>> Frameworks', [
+            'Intro to F7',
+            '>> UI Flow',
+        ]],
+        ['>> Database', [
+            'Lesson 5',
+            '>> Lesson 6'
+        ]],
     ];
 
     const cardsData = [{
-        deck: 'fundamentals',
+        deck: 'introToAppDev',
         cards: [{
-            title: 'Intro to App Dev',
+            title: 'Lesson Plan',
             sections: [{
                 content: [
-                    { type: 'p', html: '[em::Native vs. Hybrid Development] - Slides & multimedia focused on introducing app development and the different approaches, as well as the pro/con of each. Include the frameworks that will be used later on. Mention that HTML will be one of the requirements for hybrid development.' },
+                    { type: 'p', html: '[em::Native vs. Hybrid Development] - Slides & multimedia focused on introducing app development and the different approaches, as well as the pro/con of each.' },
+                    { type: 'p', html: 'Include the [em::frameworks] that will be used later on.' },
+                    { type: 'p', html: 'Mention that [em::HTML] is one of the requirements for hybrid development.' },
+                    { type: 'p', html: 'Intro to web languages and start with the basics.' },
+                ]
+            }],
+        }, {
+            title: 'Slide',
+            sections: [{
+                content: [
+                    { type: 'p', html: '[link::#]' },
+                ]
+            }],
+        }],
+    }, {
+        deck: 'webFundamental',
+        cards: [{
+            title: 'Lesson Plan',
+            sections: [{
+                content: [
+                    { type: 'p', html: 'Standard unit plan for HTML & CSS.' },
+                    { type: 'p', html: 'Cover [em::nesting], [em::id & class names], [em::CSS styling].' },
+                ]
+            }],
+        }],
+    }, {
+        deck: 'introToF7',
+        cards: [{
+            title: 'Lesson Plan',
+            sections: [{
+                content: [
+                    { type: 'p', html: 'Revisit the concept of frameworks using previous slide.' },
+                    { type: 'p', html: '[em::Setting up] Framekwork7 - sandbox template for the class to set up Framework7. Add [slides::#] as supporting material for the teacher.' },
+                    { type: 'p', html: 'Create a basic interface using the [guided project::#] on the platform.' },
                 ]
             }],
         }],
