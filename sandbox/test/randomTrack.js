88e6f6f65a18ce16e2e3c6fd48d665a7779f9e80
Siuling Ding <sd@bsdacademy.com>
2019-04-04 21:43:32 +0800

    no message

--- sandbox/test/randomTrack.js
+++ sandbox/test/randomTrack.js
@@ -2,12 +2,14 @@ var content = document.querySelector('#content');
 var grid = document.querySelector('#grid');
 var car = document.querySelector('#car');
 var btnRun = document.querySelector('#btnRun');
+var messages = document.querySelector('#messages');
+var userInput = document.querySelector('#userInput');
 var cellSize = 32;
-var nColumn = 20;
-var nRow = 10;
-var trackCells = [];
-var LongMemrory = [];
-var workingMemory = [];
+var nColumn = 10;
+var nRow = 5;
+var gridCells = [];
+
+var debugMode = true;
 
 window.onload = init;
 window.onresize = resizeContent;
@@ -23,37 +25,25 @@ function init() {
     resizeContent();
 }
 
-function autoPilot() {
-    btnRun.disabled = true;
-    btnRun.textContent = 'Ready...';
-
-    setTimeout(() => {
-        btnRun.textContent = '';
-    }, 1000);
-}
-
 function findNextCell() {
+    if (event.propertyName == 'transform') return;
+
     var cellUp = car.currentCell.up;
 
     if (cellUp && cellUp != car.prevCell && cellUp.isConnectedTo(car.currentCell)) {
-        // check through long memory
-        //   if cellUp is in longMemory && the residing entry is a success, then go ahead
         moveCarTo(cellUp);
-        workingMemory.push(cellUp);
     }
     else {
         var cellDown = car.currentCell.down;
 
         if (cellDown && cellDown != car.prevCell && cellDown.isConnectedTo(car.currentCell)) {
             moveCarTo(cellDown);
-            workingMemory.push(cellDown);
         }
         else {
             var cellRight = car.currentCell.right;
 
             if (cellRight && cellRight != car.prevCell && cellRight.isConnectedTo(car.currentCell)) {
                 moveCarTo(cellRight);
-                workingMemory.push(right);
             }
             else {
                 checkResult();
@@ -62,78 +52,122 @@ function findNextCell() {
     }
 }
 
+function checkCell(cell) {
+    
+    cell.color = cell.style.backgroundColor;
+    cell.style.backgroundColor = 'lightgreen';
+
+    if (cell && cell != car.prevCell && cell.isConnectedTo(car.currentCell)) {
+        if (debugMode) {
+            // setTimeout(cell => set);
+        }
+        else {
+            moveCarTo(cell);
+        }
+    }
+}
+
 function checkResult() {
-    if ('reached_end') {
-        LongMemrory.push(workingMemory, 'success');
-        workingMemory = [];
+    var carPositionIndex = car.currentRow.indexOf(car.currentCell);
+    
+    if (carPositionIndex == car.currentRow.length - 1) {
+        console.log('The auto-pilot system worked!');
     }
     else {
-        LongMemrory.push(workingMemory, 'failure');
+        console.log('Your auto-pilot system malfunctioned.');
     }
 }
 
-function readUserCommand(cmd) {
-    if (cmd == "stop") {
-        moveCarTo(car);
-        dashboard('car stopped.');
-    }
-    else if (cmd == "continue" || cmd == 'keep going' || cmd == 'resume') {
-        findNextCell();
-    }
-    else if (cmd == 'where am i') {
-        dashboard('We are in D4');
-    }
-}
+function moveCarTo(nextCell) {
+    //  rotate the car
+    if (car.currentCell) {
+        var rotation = 0;
+        
+        if (car.currentCell.up == nextCell) {
+            rotation = -90;
+        }
+        else if (car.currentCell.down == nextCell) {
+            rotation = 90;
+        }
 
-function fail(msg) {
-    car.removeEventListener('transitionend', nextMove);
+        car.style.transform = `scale(0.6) rotate(${rotation}deg)`;
+    }
 
-    setTimeout(() => {
-        moveCarTo(car);
-        console.log(msg);
-    }, 100);
-}
-
-function moveCarTo(el) {
     car.prevCell = car.currentCell;
-    car.currentCell = el;
-    car.style.top = el.offsetTop + 'px';
-    car.style.left = el.offsetLeft + 'px';
+    car.currentCell = nextCell;
+    car.currentRow = gridCells.filter(row => row.includes(nextCell))[0];
+    car.style.top = nextCell.offsetTop + 'px';
+    car.style.left = nextCell.offsetLeft + 'px';
+    
+    
 }
 
 function plotPath() {
-    var randomRowIndex = Math.floor(Math.random() * trackCells.length);
-    var prevCell = trackCells[randomRowIndex][0];
+    var randomRowIndex = Math.floor(Math.random() * gridCells.length);
 
-    prevCell.style.backgroundColor = 'lightblue';
-    prevCell.inDir = 'left';
+    car.currentRow = gridCells[randomRowIndex];
+
+    var prevCell = car.currentRow[0];
+    var nextCell;
+
+    prevCell.inDir = ['left'];
 
     moveCarTo(prevCell);
 
     do {
         var randomBranchIndex = Math.floor(Math.random() * prevCell.paths.length);
-        var nextCell;
 
         nextCell = prevCell.paths[randomBranchIndex];
-        nextCell.style.backgroundColor = 'lightblue';
         nextCell.paths = nextCell.paths.filter(cell => cell != prevCell);
         
         if (nextCell.offsetTop > prevCell.offsetTop) {
-            prevCell.outDir = 'down';
-            nextCell.inDir = 'up';
+            nextCell.inDir = ['up'];
+            prevCell.outDir = ['down'];
         }
         else if (nextCell.offsetTop < prevCell.offsetTop) {
-            prevCell.outDir = 'up';
-            nextCell.inDir = 'down';
+            nextCell.inDir = ['down'];
+            prevCell.outDir = ['up'];
         }
         else {
-            prevCell.outDir = 'right';
-            nextCell.inDir = 'left';
+            nextCell.inDir = ['left'];
+            prevCell.outDir = ['right'];
         }
-        
+
+        //  assign appropriate road type image
+        var roadType;
+        var dir = [prevCell.inDir.sort().join(''), prevCell.outDir.sort().join('')].sort().join('');
+
+        if (dir == 'downup') {
+            roadType = 'vertical';
+        }
+        else if (dir == 'leftright') {
+            roadType = 'horizontal';
+        }
+        else if (dir == 'downright') {
+            roadType = 'upper_left_corner';
+        }
+        else if (dir == 'downleft') {
+            roadType = 'upper_right_corner';
+        }
+        else if (dir == 'leftup') {
+            roadType = 'lower_right_corner';
+        }
+        else if (dir == 'rightup') {
+            roadType = 'lower_left_corner';
+        }
+
+        prevCell.setRoadType(roadType);
+
+        if (nextCell.endOfRow) {
+            nextCell.setRoadType('finish');
+        }
+
+        //  update prevCell for next loop iteration
         prevCell = nextCell;
     }
     while (nextCell.endOfRow == false);
+
+
 }
 
 function drawGrid() {
@@ -142,13 +176,16 @@ function drawGrid() {
 
     grid.style.width = nColumn * cellSize + 'px';
     grid.style.height = nRow * cellSize + 'px';
-    content.style.width = grid.style.width;
+
+    messages.style.height = `${grid.offsetHeight - userInput.offsetHeight}px`;
+
+    content.style.width = `${grid.offsetWidth + messages.offsetWidth}px`;
 
     //  build the trackCells 2D array
     rows.forEach((row, rowIndex) => {
         //  create an array of undefined items whose length is equal to nColumn
         row = new Array(nColumn).fill();
-        trackCells.push(row);
+        gridCells.push(row);
 
         row.forEach((cell, cellIndex) => {
             cell = document.createElement('div');
@@ -161,31 +198,35 @@ function drawGrid() {
     });
 
     //  store neighbouring cells for each cell
-    trackCells.forEach((row, nthRow) => {
+    gridCells.forEach((row, nthRow) => {
         row.forEach((cell, nthColumn) => {
             cell.up = cell.down = cell.right = null;
             cell.paths = [];
-
-            if (nthRow > 0) {
-                cell.up = trackCells[nthRow - 1][nthColumn];
-                cell.paths.push(cell.up);
-            }
-            if (nthRow < nRow - 1) {
-                cell.down = trackCells[nthRow + 1][nthColumn];
-                cell.paths.push(cell.down);
-            }
-            if (nthColumn < nColumn - 1) {
-                cell.right = trackCells[nthRow][nthColumn + 1];
-                cell.paths.push(cell.right);
-            }
-
-            cell.isConnectedTo = block => {
-                var pathUp = block.outDir == 'up' && cell.inDir == 'down';
-                var pathDown = block.outDir == 'down' && cell.inDir == 'up';
-                var pathRight = block.outDir == 'right' && cell.inDir == 'left';
+            cell.inDir = cell.outDir = [];
+            cell.isConnectedTo = prevCell => {
+                var pathUp = prevCell.outDir.includes('up') && cell.inDir.includes('down');
+                var pathDown = prevCell.outDir.includes('down') && cell.inDir.includes('up');
+                var pathRight = prevCell.outDir.includes('right') && cell.inDir.includes('left');
 
                 return pathUp || pathDown || pathRight;
             };
+            cell.setRoadType = type => {
+                cell.className = 'road';
+                cell.style.backgroundImage = `url('https://app.bsd.education/resources/road_${type}.png')`;
+            };
+
+            if (nthRow > 0) {
+                cell.up = gridCells[nthRow - 1][nthColumn];
+                cell.paths.push(cell.up);
+            }
+            if (nthRow < nRow - 1) {
+                cell.down = gridCells[nthRow + 1][nthColumn];
+                cell.paths.push(cell.down);
+            }
+            if (nthColumn < nColumn - 1) {
+                cell.right = gridCells[nthRow][nthColumn + 1];
+                cell.paths.push(cell.right);
+            }
         });
     });
 }
