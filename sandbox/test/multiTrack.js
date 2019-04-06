32d487b2f26f03c048649a82000a625545006c8a
Siuling Ding <sd@bsdacademy.com>
2019-04-06 21:32:43 +0800

    [sandbox] - backup

---
+++ sandbox/test/multiTrack.js
@@ -0,0 +1,263 @@
+var content = document.querySelector('#content');
+var grid = document.querySelector('#grid');
+var car = document.querySelector('#car');
+var btnRun = document.querySelector('#btnRun');
+var btnAddPath = document.querySelector('#btnAddPath');
+var messages = document.querySelector('#messages');
+var userInput = document.querySelector('#userInput');
+var cellSize = 32;
+var nColumn = 20;
+var nRow = 8;
+var gridCells = [];
+var longMemory = [];
+
+var debugMode = true;
+
+window.onload = init;
+window.onresize = resizeContent;
+btnRun.onclick = findNextCell;
+btnAddPath.onclick = addPathBranch;
+
+function init() {
+    drawGrid();
+    plotFirstPath();
+
+    //  handle the transitionend event using the nextCommand function
+    car.addEventListener('transitionend', findNextCell);
+
+    resizeContent();
+}
+
+function findNextCell() {
+    if (event.propertyName == 'transform') return;
+
+    var cellUp = car.currentCell.up;
+
+    if (cellUp && cellUp != car.prevCell && cellUp.isConnectedTo(car.currentCell)) {
+        moveCarTo(cellUp);
+    }
+    else {
+        var cellDown = car.currentCell.down;
+
+        if (cellDown && cellDown != car.prevCell && cellDown.isConnectedTo(car.currentCell)) {
+            moveCarTo(cellDown);
+        }
+        else {
+            var cellRight = car.currentCell.right;
+
+            if (cellRight && cellRight != car.prevCell && cellRight.isConnectedTo(car.currentCell)) {
+                moveCarTo(cellRight);
+            }
+            else {
+                checkResult();
+            }
+        }
+    }
+}
+
+function checkCell(cell) {
+    
+    cell.color = cell.style.backgroundColor;
+    cell.style.backgroundColor = 'lightgreen';
+
+    if (cell && cell != car.prevCell && cell.isConnectedTo(car.currentCell)) {
+        if (debugMode) {
+            // setTimeout(cell => set);
+        }
+        else {
+            moveCarTo(cell);
+        }
+    }
+}
+
+function checkResult() {
+    var carPositionIndex = car.currentRow.indexOf(car.currentCell);
+    
+    if (carPositionIndex == car.currentRow.length - 1) {
+        console.log('The auto-pilot system worked!');
+    }
+    else {
+        console.log('Your auto-pilot system malfunctioned.');
+    }
+}
+
+function moveCarTo(nextCell) {
+    //  rotate the car
+    if (car.currentCell) {
+        var rotation = 0;
+        
+        if (car.currentCell.up == nextCell) {
+            rotation = -90;
+        }
+        else if (car.currentCell.down == nextCell) {
+            rotation = 90;
+        }
+
+        car.style.transform = `rotate(${rotation}deg)`;
+    }
+
+    car.prevCell = car.currentCell;
+    car.currentCell = nextCell;
+    car.currentRow = gridCells.filter(row => row.includes(nextCell))[0];
+    car.style.top = nextCell.offsetTop + 'px';
+    car.style.left = nextCell.offsetLeft + 'px';
+    
+    
+}
+
+function addPathBranch() {
+    var roads = gridCells.flat().filter(cell => cell.id != 'finish' && cell.className.includes('road'));
+    var randomRoadIndex = Math.floor(Math.random() * roads.length);
+    var prevRoad = roads[randomRoadIndex];
+    var newRoad;
+    
+    do {
+        prevRoad.paths = prevRoad.paths.filter(cell => cell.className == '');
+
+        var randomBranchIndex = Math.floor(Math.random() * prevRoad.paths.length);
+
+        newRoad = prevRoad.paths[randomBranchIndex];
+        newRoad.paths = newRoad.paths.filter(cell => cell != prevRoad);
+
+        if (newRoad.offsetTop > prevRoad.offsetTop) {
+            prevRoad.outDir.push('down');
+            newRoad.inDir.push('up');
+        }
+        else if (newRoad.offsetTop < prevRoad.offsetTop) {
+            prevRoad.outDir.push('up');
+            newRoad.inDir.push('down');
+        }
+        else {
+            prevRoad.outDir.push('right');
+            newRoad.inDir.push('left');
+        }
+
+        //  assign appropriate road type image
+        var roadType = prevRoad.inDir.concat(prevRoad.outDir).sort().join('');
+
+        prevRoad.setRoadType(roadType);
+
+        prevRoad = newRoad;
+    }
+    while (newRoad.className == '');
+}
+
+function plotFirstPath() {
+    var randomRowIndex = Math.floor(Math.random() * gridCells.length);
+
+    car.currentRow = gridCells[randomRowIndex];
+
+    var prevRoad = car.currentRow[0];
+    var newRoad;
+
+    prevRoad.inDir.push('left');
+
+    moveCarTo(prevRoad);
+
+    do {
+        var randomBranchIndex = Math.floor(Math.random() * prevRoad.paths.length);
+
+        newRoad = prevRoad.paths[randomBranchIndex];
+        newRoad.paths = newRoad.paths.filter(cell => cell != prevRoad);
+        
+        if (newRoad.offsetTop > prevRoad.offsetTop) {
+            prevRoad.outDir.push('down');
+            newRoad.inDir.push('up');
+        }
+        else if (newRoad.offsetTop < prevRoad.offsetTop) {
+            prevRoad.outDir.push('up');
+            newRoad.inDir.push('down');
+        }
+        else {
+            prevRoad.outDir.push('right');
+            newRoad.inDir.push('left');
+        }
+
+        //  assign appropriate road type image
+        var roadType = prevRoad.inDir.concat(prevRoad.outDir).sort().join('');
+
+        prevRoad.setRoadType(roadType);
+
+        if (newRoad.endOfRow) {
+            newRoad.setRoadType('finish');
+            newRoad.id = 'finish';
+        }
+
+        //  update prevCell for next loop iteration
+        prevRoad = newRoad;
+    }
+    while (newRoad.endOfRow == false);
+}
+
+function drawGrid() {
+    //  create an array or undefined items whose length is equal to nRow
+    var rows = new Array(nRow).fill();
+
+    grid.style.width = nColumn * cellSize + 'px';
+    grid.style.height = nRow * cellSize + 'px';
+
+    messages.style.height = `${grid.offsetHeight - userInput.offsetHeight}px`;
+
+    content.style.width = `${grid.offsetWidth + messages.offsetWidth}px`;
+
+    //  build the trackCells 2D array
+    rows.forEach((row, rowIndex) => {
+        //  create an array of undefined items whose length is equal to nColumn
+        row = new Array(nColumn).fill();
+        gridCells.push(row);
+
+        row.forEach((cell, cellIndex) => {
+            cell = document.createElement('div');
+            cell.style.top = rowIndex * cellSize + 'px';
+            cell.style.left = cellIndex * cellSize + 'px';
+            cell.endOfRow = cellIndex == nColumn - 1;      
+            row[cellIndex] = cell;
+            grid.appendChild(cell);
+        });
+    });
+
+    //  store neighbouring cells for each cell
+    gridCells.forEach((row, nthRow) => {
+        row.forEach((cell, nthColumn) => {
+            cell.up = null;
+            cell.down = null;
+            cell.right = null;
+            cell.paths = [];
+            cell.inDir = [];
+            cell.outDir = [];
+            cell.isConnectedTo = prevCell => {
+                var pathUp = prevCell.outDir.includes('up') && cell.inDir.includes('down');
+                var pathDown = prevCell.outDir.includes('down') && cell.inDir.includes('up');
+                var pathRight = prevCell.outDir.includes('right') && cell.inDir.includes('left');
+
+                return pathUp || pathDown || pathRight;
+            };
+            cell.setRoadType = type => {
+                cell.className = 'road';
+                cell.style.backgroundImage = `url('https://app.bsd.education/resources/road_${type}.png')`;
+            };
+
+            if (nthRow > 0) {
+                cell.up = gridCells[nthRow - 1][nthColumn];
+                cell.paths.push(cell.up);
+            }
+            if (nthRow < nRow - 1) {
+                cell.down = gridCells[nthRow + 1][nthColumn];
+                cell.paths.push(cell.down);
+            }
+            if (nthColumn < nColumn - 1) {
+                cell.right = gridCells[nthRow][nthColumn + 1];
+                cell.paths.push(cell.right);
+            }
+        });
+    });
+}
+
+function resizeContent() {
+    var contentRatio = content.offsetWidth / content.offsetHeight;
+    var windowRatio = window.innerWidth / window.innerHeight;
+
+    content.style.left = `${(window.innerWidth - content.offsetWidth) / 2}px`;
+    content.style.top = `${(window.innerHeight - content.offsetHeight) / 2}px`;
+    content.style.transform = `scale(${contentRatio > windowRatio ? window.innerWidth / content.offsetWidth : window.innerHeight / content.offsetHeight})`;
+}
