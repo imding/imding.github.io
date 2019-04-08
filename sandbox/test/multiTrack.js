25daa5cfb765bde3109525c8458c4f1838952978
imding <minddevoursall@gmail.com>
2019-04-08 12:12:23 +0800

    [sandbox] - auto drive car sim updated

--- sandbox/test/multiTrack.js
+++ sandbox/test/multiTrack.js
@@ -6,21 +6,28 @@ var btnAddPath = document.querySelector('#btnAddPath');
 var messages = document.querySelector('#messages');
 var userInput = document.querySelector('#userInput');
 var cellSize = 32;
-var nColumn = 20;
-var nRow = 8;
+var nColumn = 10;
+var nRow = 10;
 var gridCells = [];
-var longMemory = [];
-
-var debugMode = true;
+var bestAttempt = [];
+var shortMemory = [];
+var mode = 'fast';
 
 window.onload = init;
 window.onresize = resizeContent;
 btnRun.onclick = findNextCell;
-btnAddPath.onclick = addPathBranch;
 
 function init() {
     drawGrid();
-    plotFirstPath();
+    
+    //  set starting conditions for the car
+    car.className = mode;
+    car.rotation = 0;
+    car.currentRow = randomItemFrom(gridCells);
+    car.startCell = car.currentRow[0];
+    moveCarTo(car.startCell);
+
+    plotPath();
 
     //  handle the transitionend event using the nextCommand function
     car.addEventListener('transitionend', findNextCell);
@@ -28,165 +35,222 @@ function init() {
     resizeContent();
 }
 
+function stopAlgorithm() {
+    setTimeout(() => {
+        shortMemory = [];
+
+        // btnRun.disabled = false;
+        // btnRun.textContent = 'Reset Car';
+        // btnRun.onclick = resetCar;
+        resetCar();
+    }, 500);
+}
+
 function findNextCell() {
-    if (event.propertyName == 'transform') return;
+    if (event && event.propertyName == 'transform') return;
+    if (car.currentCell.id == 'finish') return;
 
-    var cellUp = car.currentCell.up;
+    btnRun.disabled = true;
+    btnRun.textContent = 'Running...';
 
-    if (cellUp && cellUp != car.prevCell && cellUp.isConnectedTo(car.currentCell)) {
-        moveCarTo(cellUp);
+    var randomNextCell;
+    var currentCell = car.currentCell;
+    
+    //  check if the car has circled back to the starting cell
+    if (shortMemory.length > 0 && currentCell == car.startCell && currentCell.roadType.length == 2) {
+        console.log(`The algorithm failed to find the exit in this iteration. Distance travelled: ${shortMemory.length} cells.`);
+        return stopAlgorithm();
     }
-    else {
-        var cellDown = car.currentCell.down;
 
-        if (cellDown && cellDown != car.prevCell && cellDown.isConnectedTo(car.currentCell)) {
-            moveCarTo(cellDown);
-        }
-        else {
-            var cellRight = car.currentCell.right;
-
-            if (cellRight && cellRight != car.prevCell && cellRight.isConnectedTo(car.currentCell)) {
-                moveCarTo(cellRight);
-            }
-            else {
-                checkResult();
-            }
-        }
+    while (randomNextCell == undefined || randomNextCell == car.prevCell) {        
+        randomNextCell = currentCell[randomItemFrom(currentCell.roadType)];
     }
+    
+    shortMemory.push(randomNextCell);
+    
+    //  check if the car has reached the exit
+    if (randomNextCell.id == 'finish') {
+        console.log(`Your algorithm worked! Total distance travelled: ${shortMemory.length} cells.`);
+        bestAttempt = shortMemory;
+        stopAlgorithm();
+    }
+    
+    //  check if the car has travelled same distance as the best attempt
+    else if (shortMemory.length == bestAttempt.length) {
+        console.log(`The algorithm failed to find the exit in less than ${bestAttempt.length} moves, this iteration is discarded.`);
+        return stopAlgorithm();
+    }
+
+    moveCarTo(randomNextCell);
 }
 
-function checkCell(cell) {
-    
-    cell.color = cell.style.backgroundColor;
-    cell.style.backgroundColor = 'lightgreen';
-
-    if (cell && cell != car.prevCell && cell.isConnectedTo(car.currentCell)) {
-        if (debugMode) {
-            // setTimeout(cell => set);
+function resetCar() {    
+    var countdown = duration => {
+        if (duration > 0) {
+            btnRun.disabled = true;
+            btnRun.textContent = `Restarting in ${duration}`;
+            return setTimeout(() => countdown(--duration), 1000);
         }
-        else {
-            moveCarTo(cell);
-        }
-    }
-}
 
-function checkResult() {
-    var carPositionIndex = car.currentRow.indexOf(car.currentCell);
+        car.style.transition = transition;
+        // btnRun.disabled = false;
+        // btnRun.textContent = 'Run Again';
+        // btnRun.onclick = findNextCell;
+        findNextCell();
+    };
+    var transition = car.style.transition;
     
-    if (carPositionIndex == car.currentRow.length - 1) {
-        console.log('The auto-pilot system worked!');
-    }
-    else {
-        console.log('Your auto-pilot system malfunctioned.');
-    }
+    car.style.transition = 'none';
+    car.rotation = 0;
+    car.prevCell = null;
+    car.currentCell = null;
+    
+    moveCarTo(car.startCell);
+    countdown(1);
 }
 
 function moveCarTo(nextCell) {
     //  rotate the car
-    if (car.currentCell) {
-        var rotation = 0;
+    var currentCell = car.currentCell;
+    var prevCell = car.prevCell;
+
+    if (currentCell) {
+        var clockwise;
+        var anitClockwise;
+        var cellUp = currentCell.up;
+        var cellDown = currentCell.down;
+        var cellRight = currentCell.right;
+        var cellLeft = currentCell.left;
         
-        if (car.currentCell.up == nextCell) {
-            rotation = -90;
+        if (prevCell) {
+            clockwise =
+                (prevCell == cellRight && nextCell == cellUp) ||
+                (prevCell == cellLeft && nextCell == cellDown) ||
+                (prevCell == cellUp && nextCell == cellLeft) ||
+                (prevCell == cellDown && nextCell == cellRight);
+            
+            anitClockwise =
+                (prevCell == cellRight && nextCell == cellDown) ||
+                (prevCell == cellLeft && nextCell == cellUp) ||
+                (prevCell == cellUp && nextCell == cellRight) ||
+                (prevCell == cellDown && nextCell == cellLeft);
         }
-        else if (car.currentCell.down == nextCell) {
-            rotation = 90;
+        else {
+            clockwise = nextCell == cellDown;
+            anitClockwise = nextCell == cellUp;
         }
 
-        car.style.transform = `rotate(${rotation}deg)`;
+        if (clockwise) {
+            car.rotation += 90;
+        }
+        else if (anitClockwise) {
+            car.rotation -= 90;
+        }
     }
-
+    
     car.prevCell = car.currentCell;
     car.currentCell = nextCell;
     car.currentRow = gridCells.filter(row => row.includes(nextCell))[0];
     car.style.top = nextCell.offsetTop + 'px';
     car.style.left = nextCell.offsetLeft + 'px';
-    
-    
+    car.style.transform = `rotate(${car.rotation}deg)`;
 }
 
-function addPathBranch() {
-    var roads = gridCells.flat().filter(cell => cell.id != 'finish' && cell.className.includes('road'));
-    var randomRoadIndex = Math.floor(Math.random() * roads.length);
-    var prevRoad = roads[randomRoadIndex];
-    var newRoad;
-    
-    do {
-        prevRoad.paths = prevRoad.paths.filter(cell => cell.className == '');
+function plotPath(prevRoadCell) {
+    var newRoadCell;
 
-        var randomBranchIndex = Math.floor(Math.random() * prevRoad.paths.length);
+    prevRoadCell = prevRoadCell || car.currentCell;
+    prevRoadCell.roadType.push('left');
 
-        newRoad = prevRoad.paths[randomBranchIndex];
-        newRoad.paths = newRoad.paths.filter(cell => cell != prevRoad);
+    var expandFrom = targetCell => {
+        //  remove target cell from the available paths of the previous road cell
+        remove(targetCell).from(prevRoadCell.paths);
+        //  remove the previous road cell from the available paths of target cell
+        remove(prevRoadCell).from(targetCell.paths);
 
-        if (newRoad.offsetTop > prevRoad.offsetTop) {
-            prevRoad.outDir.push('down');
-            newRoad.inDir.push('up');
+        //  assign road type for previous and target cells
+        if (targetCell.up == prevRoadCell) {
+            prevRoadCell.roadType.push('down');
+            targetCell.roadType.push('up');
         }
-        else if (newRoad.offsetTop < prevRoad.offsetTop) {
-            prevRoad.outDir.push('up');
-            newRoad.inDir.push('down');
+        else if (targetCell.down == prevRoadCell) {
+            prevRoadCell.roadType.push('up');
+            targetCell.roadType.push('down');
+        }
+        else if (targetCell.left == prevRoadCell) {
+            prevRoadCell.roadType.push('right');
+            targetCell.roadType.push('left');
         }
         else {
-            prevRoad.outDir.push('right');
-            newRoad.inDir.push('left');
+            prevRoadCell.roadType.push('left');
+            targetCell.roadType.push('right');
         }
 
         //  assign appropriate road type image
-        var roadType = prevRoad.inDir.concat(prevRoad.outDir).sort().join('');
+        prevRoadCell.setRoadType(prevRoadCell.roadType.sort().join(''));
 
-        prevRoad.setRoadType(roadType);
+        if (targetCell.className == 'road') {
+            targetCell.setRoadType(targetCell.roadType.sort().join(''));
+        }
 
-        prevRoad = newRoad;
-    }
-    while (newRoad.className == '');
-}
-
-function plotFirstPath() {
-    var randomRowIndex = Math.floor(Math.random() * gridCells.length);
-
-    car.currentRow = gridCells[randomRowIndex];
-
-    var prevRoad = car.currentRow[0];
-    var newRoad;
-
-    prevRoad.inDir.push('left');
-
-    moveCarTo(prevRoad);
-
+        prevRoadCell = targetCell;
+    };
+    
+    //  expand from the previous road cell until end of row is reached
     do {
-        var randomBranchIndex = Math.floor(Math.random() * prevRoad.paths.length);
-
-        newRoad = prevRoad.paths[randomBranchIndex];
-        newRoad.paths = newRoad.paths.filter(cell => cell != prevRoad);
+        newRoadCell = randomItemFrom(prevRoadCell.paths.filter(cell => cell != prevRoadCell.left));
+        expandFrom(newRoadCell);
         
-        if (newRoad.offsetTop > prevRoad.offsetTop) {
-            prevRoad.outDir.push('down');
-            newRoad.inDir.push('up');
-        }
-        else if (newRoad.offsetTop < prevRoad.offsetTop) {
-            prevRoad.outDir.push('up');
-            newRoad.inDir.push('down');
+        newRoadCell.paths.forEach(neighbourCell => {
+            //  check whether target cell blocks off paths of neighbouring cells
+            if (neighbourCell.className == 'road') {
+                remove(newRoadCell).from(neighbourCell.paths);
+                remove(neighbourCell).from(newRoadCell.paths);
+            }
+        });
+    }
+    while (newRoadCell.endOfRow == false);
+
+    //  set road type on the last cell in a row
+    newRoadCell.setRoadType('finish');
+    newRoadCell.id = 'finish';
+
+    var branch = [];
+    var emptyCells = gridCells.flat().filter(cell => cell.className == '' && cell.right != null);
+
+    while (emptyCells.length > 0) {
+        if (branch.length == 0) {
+            var branchCell = randomItemFrom(gridCells.flat().filter(cell =>
+                cell.className == 'road' &&
+                cell.id != 'finish' &&
+                cell.right != null &&
+                cell.right.right != null &&
+                cell.right.id != 'finish' &&
+                cell.paths.filter(neighbourCell => neighbourCell.className == '').length > 0
+            ));
+
+            prevRoadCell = branchCell;
+            newRoadCell = randomItemFrom(branchCell.paths.filter(cell => cell.className == ''));
         }
         else {
-            prevRoad.outDir.push('right');
-            newRoad.inDir.push('left');
+            newRoadCell = randomItemFrom(branch[branch.length - 1].paths);
+            emptyCells.pop();
         }
 
-        //  assign appropriate road type image
-        var roadType = prevRoad.inDir.concat(prevRoad.outDir).sort().join('');
-
-        prevRoad.setRoadType(roadType);
-
-        if (newRoad.endOfRow) {
-            newRoad.setRoadType('finish');
-            newRoad.id = 'finish';
+        //  remove paths that lead to the last column
+        if (newRoadCell.right.right == null) {
+            remove(newRoadCell.right).from(newRoadCell.paths);
         }
 
-        //  update prevCell for next loop iteration
-        prevRoad = newRoad;
+        expandFrom(newRoadCell);
+
+        if (newRoadCell.className == 'road') {
+            branch = [];
+        }
+        else {
+            branch.push(newRoadCell);
+        }
     }
-    while (newRoad.endOfRow == false);
 }
 
 function drawGrid() {
@@ -221,16 +285,20 @@ function drawGrid() {
         row.forEach((cell, nthColumn) => {
             cell.up = null;
             cell.down = null;
+            cell.left = null;
             cell.right = null;
             cell.paths = [];
-            cell.inDir = [];
-            cell.outDir = [];
-            cell.isConnectedTo = prevCell => {
-                var pathUp = prevCell.outDir.includes('up') && cell.inDir.includes('down');
-                var pathDown = prevCell.outDir.includes('down') && cell.inDir.includes('up');
-                var pathRight = prevCell.outDir.includes('right') && cell.inDir.includes('left');
-
-                return pathUp || pathDown || pathRight;
+            cell.roadType = [];
+            cell.path = cellDir => {
+                return {
+                    linksTo: targetCell => {
+                        return {
+                            path: targetCellDir => {
+                                return cell.roadType.includes(cellDir) && targetCell.roadType.includes(targetCellDir);
+                            },
+                        };
+                    },
+                };
             };
             cell.setRoadType = type => {
                 cell.className = 'road';
@@ -244,6 +312,10 @@ function drawGrid() {
             if (nthRow < nRow - 1) {
                 cell.down = gridCells[nthRow + 1][nthColumn];
                 cell.paths.push(cell.down);
+            }
+            if (nthColumn > 0) {
+                cell.left = gridCells[nthRow][nthColumn - 1];
+                cell.paths.push(cell.left);
             }
             if (nthColumn < nColumn - 1) {
                 cell.right = gridCells[nthRow][nthColumn + 1];
@@ -260,4 +332,18 @@ function resizeContent() {
     content.style.left = `${(window.innerWidth - content.offsetWidth) / 2}px`;
     content.style.top = `${(window.innerHeight - content.offsetHeight) / 2}px`;
     content.style.transform = `scale(${contentRatio > windowRatio ? window.innerWidth / content.offsetWidth : window.innerHeight / content.offsetHeight})`;
+}
+
+function randomIndexFrom(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
+
+function randomItemFrom(arr) {
+    return arr[randomIndexFrom(arr)];
+}
+
+function remove(item) {
+    return {
+        from: arr => arr.splice(arr.indexOf(item), 1),
+    };
 }
