29497f7f209744f53eba5e7d8b7f2655188e9ccd
Siuling Ding <sd@bsdacademy.com>
2019-08-12 20:30:25 +0800

    trying monaco editor

--- monaco/src/index.js
+++ monaco/src/index.js
@@ -1,11 +1,128 @@
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-const editor = monaco.editor.create(document.getElementById('container'), {
-    value: 'console.log("Hello, world");',
-    language: 'javascript',
-    theme: 'vs-dark'
+// const editables = [
+//     {
+//         lineNumber: 3,
+//         editableRegions: [{
+//             start: 6,
+//             end: 8
+//         }]
+//     }
+// ];
+
+monaco.editor.defineTheme('dark', {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [
+        // { token: '', foreground: '5c6773' },
+        // { token: 'invalid', foreground: 'ff3333' },
+        // { token: 'emphasis', fontStyle: 'italic' },
+        // { token: 'strong', fontStyle: 'bold' },
+        // { token: 'identifier.js', foreground: '9CDCFE' },
+        // { token: 'variable.predefined', foreground: '5c6773' },
+        // { token: 'constant', foreground: 'f08c36' },
+        // { token: 'comment', foreground: 'abb0b6', fontStyle: 'italic' },
+        // { token: 'number', foreground: 'f08c36' },
+        // { token: 'number.hex', foreground: 'f08c36' },
+        // { token: 'regexp', foreground: '4dbf99' },
+        // { token: 'annotation', foreground: '41a6d9' },
+        // { token: 'type', foreground: '41a6d9' },
+        // { token: 'delimiter', foreground: '5c6773' },
+        // { token: 'delimiter.html', foreground: '5c6773' },
+        // { token: 'delimiter.xml', foreground: '5c6773' },
+        // { token: 'tag', foreground: 'e7c547' },
+        // { token: 'tag.id.jade', foreground: 'e7c547' },
+        // { token: 'tag.class.jade', foreground: 'e7c547' },
+        // { token: 'meta.scss', foreground: 'e7c547' },
+        // { token: 'metatag', foreground: 'e7c547' },
+        // { token: 'metatag.content.html', foreground: '86b300' },
+        // { token: 'metatag.html', foreground: 'e7c547' },
+        // { token: 'metatag.xml', foreground: 'e7c547' },
+        // { token: 'metatag.php', fontStyle: 'bold' },
+        // { token: 'key', foreground: '41a6d9' },
+        // { token: 'string.key.json', foreground: '41a6d9' },
+        // { token: 'string.value.json', foreground: '86b300' },
+        // { token: 'attribute.name', foreground: 'f08c36' },
+        // { token: 'attribute.value', foreground: '0451A5' },
+        // { token: 'attribute.value.number', foreground: 'abb0b6' },
+        // { token: 'attribute.value.unit', foreground: '86b300' },
+        // { token: 'attribute.value.html', foreground: '86b300' },
+        // { token: 'attribute.value.xml', foreground: '86b300' },
+        // { token: 'string', foreground: '86b300' },
+        // { token: 'string.html', foreground: '86b300' },
+        // { token: 'string.sql', foreground: '86b300' },
+        // { token: 'string.yaml', foreground: '86b300' },
+        // { token: 'keyword', foreground: 'f2590c' },
+        // { token: 'keyword.json', foreground: 'f2590c' },
+        // { token: 'keyword.flow', foreground: 'f2590c' },
+        // { token: 'keyword.flow.scss', foreground: 'f2590c' },
+        // { token: 'operator.scss', foreground: '666666' }, //
+        // { token: 'operator.sql', foreground: '778899' }, //
+        // { token: 'operator.swift', foreground: '666666' }, //
+        // { token: 'predefined.sql', foreground: 'FF00FF' }, //
+    ],
+    colors: {
+    },
 });
 
-editor.onKeyDown(() => {
-    console.log(editor.getValue());
+monaco.editor.setTheme('ace');
+
+const editor = monaco.editor.create(document.getElementById('container'), {
+    language: 'javascript',
+    theme: 'dark',
 });
+
+
+
+editor.setValue(`var n = 42;
+
+n += #BEGIN_EDITABLE#21#END_EDITABLE#;
+
+console.log(n);`);
+
+const btnEditAnswers = document.querySelector('#editAnswers');
+const info = document.querySelector('#info');
+
+let showAnswers = false;
+
+btnEditAnswers.onclick = () => {
+    if (showAnswers) {
+        //  build editables array
+
+        //  parse and add opacity class
+
+        console.log(editor.getModel());
+
+        //  replace editor.value
+        //  enable read-only
+    }
+    //  else
+    //  insert array
+    //  disable read-only
+
+    showAnswers = !showAnswers;
+
+    console.log(monaco.editor.tokenize(editor.getValue(), 'javascript'));
+};
+
+editor.onDidChangeCursorSelection(() => {
+    //  if (showing markup)
+    //  return
+    //  else if (target span is opaque)
+    //  disable read-only
+    //  else
+    //  enable read-only
+
+    let msg = `line: ${editor.getPosition().lineNumber}, column: ${editor.getPosition().column}`;
+
+    info.textContent = msg;
+});
+
+editor.onDidChangeModelContent(() => {
+    //  update editable array
+});
+
+function setOpacity(el, val) {
+    el.classList.add('read-only');
+    el.style.opacity = val;
+}
